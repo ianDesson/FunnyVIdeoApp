f9658a4634815def911983de64b8f1e7e6d65554
ianDesson <ian.desson@outlook.com>
2019-01-30 21:14:59 -0500

    Added Firebase dependencies

--- app/src/main/java/com/ian/desson/view/MainActivity.java
+++ app/src/main/java/com/ian/desson/view/MainActivity.java
@@ -1,13 +1,26 @@
-package com.ian.desson;
+package com.ian.desson.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.ian.desson.model.Video;
+import com.ian.desson.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseDatabase;
+    private DatabaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void randomVideoButton(View view) {
+
     }
 }
