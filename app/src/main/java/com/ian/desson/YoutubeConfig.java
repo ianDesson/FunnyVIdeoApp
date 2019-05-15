5f6ff9871363af928ca5e1a3aed45a2110304c8c
Ian Desson <25931837+ianDesson@users.noreply.github.com>
2019-05-14 21:54:11 -0400

    removed API Key

--- app/src/main/java/com/ian/desson/YoutubeConfig.java
+++ app/src/main/java/com/ian/desson/YoutubeConfig.java
@@ -2,7 +2,7 @@ package com.ian.desson;
 
 public class YoutubeConfig {
 
-    private static final String API_KEY = "AIzaSyCQWTifoQ1T7SBVJFT6nCffsxsa00c6JKk";
+    private static final String API_KEY = "REDACTED";
 
     public YoutubeConfig() {
 
